71c8ca8083e1ceab4cdf420f801b2c1a4ef6e595
Yann Boyongo <yaboyon3@gmail.com>
2023-12-13 10:16:52 +0200

    adding broadcast view code, adding receive view code, adding index view code and creating a styles.css page to add some styles to index view

---
+++ resources/views/broadcast.blade.php
@@ -0,0 +1,5 @@
+<div class="right message">
+    <p>{{ $message }}</p>
+    <img src="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg"
+        alt="Avatar">
+</div>
--- resources/views/receive.blade.php
+++ resources/views/receive.blade.php
@@ -1,1 +1,5 @@
-
+<div class="left message">
+    <img src="https://png.pngtree.com/png-vector/20191101/ourmid/pngtree-cartoon-color-simple-male-avatar-png-image_1934459.jpg"
+        alt="Avatar">
+    <p>{{ $message }}</p>
+</div>
